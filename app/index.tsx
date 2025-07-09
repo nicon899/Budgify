7e2c568edf7d03c09ec2435f35b4dd4e8541cdba
Nico Neubig <nico@neubig.name>
2025-07-09 21:09:35 +0200

    First Screen

---
+++ app/index.tsx
@@ -0,0 +1,205 @@
+import React, { useState } from 'react';
+import { Dimensions, StyleSheet, TouchableOpacity, View } from 'react-native';
+// import CategoryItemList from '../components/CategoryItemList';
+// import DatePicker from '../components/DatePicker';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+// import { finContext } from '../contexts/FinContext';
+
+const CategoryScreen = props => {
+    // const context = useContext(finContext);
+    const [date, _setDate] = useState(new Date());
+    // const [selectedCategory, setSelectedCategory] = useState(context.categories[0]);
+    // const childCategories = selectedCategory ? context.categories.filter((category) => category.id !== null && category.parentId === selectedCategory.id) : [];
+
+    const scaleFontSize = (fontSize: number) => {
+        return Math.ceil((fontSize * Math.min(Dimensions.get('window').width / 411, Dimensions.get('window').height / 861)));
+    }
+
+    // useEffect(() => {
+    //     setSelectedCategory(context.categories.find(c => c.id === selectedCategory.id))
+    // }, [context.categories])
+
+    // useEffect(() => {
+    //     setLatestDate();
+    // }, []);
+
+    // const setDate = (date: Date) => {
+    //     _setDate(date);
+    //     context.actions.refresh(date.toISOString())
+    // }
+
+    // const setLatestDate = () => {
+    //     let today = new Date();
+    //     if (context.transactions[0]) {
+    //         let newDate = new Date(context.transactions[0].date);
+    //         setDate(newDate > today ? newDate : today);
+    //     } else {
+    //         setDate(today);
+    //     }
+    // }
+
+    // useEffect(() => {
+    //     const backAction = () => {
+    //         if (!props.navigation.isFocused()) {
+    //             return false;
+    //         }
+    //         if (selectedCategory.id === null) {
+    //             return false;
+    //         } else {
+    //             setSelectedCategory(context.categories.find((category) => category.id === selectedCategory.parentId));
+    //             return true;
+    //         }
+    //     };
+    //     const backHandler = BackHandler.addEventListener(
+    //         "hardwareBackPress",
+    //         backAction
+    //     );
+    //     return () => {
+    //         backHandler.remove()
+    //     };
+    // }, [selectedCategory]);
+
+    return (
+        <View style={styles.screen}>
+            <View style={styles.topBar}>
+                <View style={styles.dateBar}>
+                    {/* <DatePicker
+                        style={styles.dateInput}
+                        date={date}
+                        setDate={setDate}
+                        setTime={false}
+                        showArrow={false}
+                    /> */}
+                    <View style={styles.topBarDateIcons}>
+                        {/* <TouchableOpacity
+                            style={{ alignItems: 'center', justifyContent: 'center' }}
+                            onPress={() => setDate(new Date())}
+                        >
+                            <MaterialCommunityIcons name="timetable" size={scaleFontSize(24)} color="white" />
+                            <Text style={{ color: 'white', fontSize: scaleFontSize(7) }}>Today</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={{ alignItems: 'center', justifyContent: 'center', marginLeft: 10 }}
+                            onPress={() => setLatestDate()}>
+                            <MaterialCommunityIcons name="timer-sand-full" size={scaleFontSize(24)} color="white" />
+                            <Text style={{ color: 'white', fontSize: scaleFontSize(7) }}>Latest</Text>
+                        </TouchableOpacity> */}
+                    </View>
+                </View>
+                <TouchableOpacity
+                    onPress={() => props.navigation.navigate('Settings')}>
+                    <MaterialCommunityIcons name="cog-outline" size={scaleFontSize(28)} color="white" />
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.header}>
+                {/* <TouchableOpacity
+                    style={styles.headerCat}
+                    onPress={() => {
+                        props.navigation.navigate('EditCategory', { categoryId: selectedCategory.id, name: selectedCategory.name })
+                    }}
+                >
+                    <Text style={{ color: 'white', fontSize: scaleFontSize(36), fontWeight: 'bold', textAlign: 'center' }}>{selectedCategory.name}</Text>
+                    <Text numberOfLines={1} style={{ marginRight: 5, fontSize: scaleFontSize(28), textAlign: 'center', color: selectedCategory.value > 0 ? 'green' : 'red', fontFamily: 'JetBrainsMono-Bold' }}>{(selectedCategory.name + selectedCategory.value).length > 20 && '\n'}{selectedCategory.value.toFixed(2)}</Text>
+                </TouchableOpacity> */}
+            </View>
+
+            <View style={{ flex: 1 }}>
+                {/* <CategoryItemList
+                    style={{ maxHeight: '100%' }}
+                    bookings={context.transactions.filter(t => t.categoryId === selectedCategory.id && t.date <= date)}
+                    categories={childCategories}
+                    showBooking={(id) => props.navigation.push('Booking', { id: id })}
+                    showCategory={(id) => setSelectedCategory(context.categories.find((category) => category.id === id))}
+                    showBookings={selectedCategory.id !== null}
+                /> */}
+            </View>
+
+            <View style={styles.transBar}>
+                {/* {selectedCategory.id !== null && <TouchableOpacity
+                    style={[styles.transButton, { borderColor: '#00FF00', backgroundColor: '#00FF00' }]}
+                    onPress={() => {
+                        props.navigation.navigate('CreateBooking', {
+                            categoryId: selectedCategory.id, editMode: false, value: 1
+                        });
+                    }}
+                >
+                    <Text style={{ color: 'white' }}>+</Text>
+                </TouchableOpacity>}
+                {selectedCategory.id !== null && <TouchableOpacity
+                    style={[styles.transButton, { borderColor: '#FF0000', backgroundColor: '#FF0000' }]}
+                    onPress={() => {
+                        props.navigation.navigate('CreateBooking', {
+                            categoryId: selectedCategory.id, editMode: false, value: -1
+                        });
+                    }}
+                >
+                    <Text style={{ color: 'white' }}>-</Text>
+                </TouchableOpacity>} */}
+            </View>
+        </View >
+    );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        backgroundColor: 'black',
+        color: 'white',
+        padding: 5
+    },
+    header: {
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        borderColor: 'grey',
+    },
+    topBar: {
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: 10,
+        paddingVertical: 5
+    },
+    dateBar: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: '65%'
+    },
+    dateInput: {
+        flex: 1
+    },
+    topBarDateIcons: {
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        flexDirection: 'row',
+        paddingHorizontal: 5
+    },
+    headerCat: {
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        justifyContent: 'space-between',
+        paddingVertical: 2,
+        paddingHorizontal: 5,
+    },
+    transBar: {
+        flexDirection: 'row',
+        width: '100%',
+    },
+    transButton: {
+        borderWidth: 1,
+        borderColor: 'red',
+        borderRadius: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10,
+        margin: 10,
+        flex: 1,
+    },
+});
+
+export default CategoryScreen;
---
+++ contexts/FinContext.js
@@ -0,0 +1,300 @@
+import { Asset } from 'expo-asset';
+import * as FileSystem from 'expo-file-system';
+import * as SQLite from "expo-sqlite";
+import React, { useEffect, useRef, useState } from 'react';
+
+const formatValue = (num) => Math.round((num + Number.EPSILON) * 100) / 100
+
+async function openDatabase() {
+    if (!(await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'SQLite')).exists) {
+        await FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'SQLite');
+        await FileSystem.downloadAsync(
+            Asset.fromModule(require('../assets/finDatabase.db')).uri,
+            FileSystem.documentDirectory + 'SQLite/finDatabase.db'
+        );
+    }
+    const db = await SQLite.openDatabaseAsync('finDatabase.db');
+    db._db.exec(
+        [{ sql: 'PRAGMA foreign_keys = ON;', args: [] }],
+        false,
+        () => {},
+    );
+    return db;
+}
+
+const initialContext = {
+    actions: {
+        addCategory: async (category) => { },
+        updateCategory: async (category) => { },
+        deleteCategory: async (id) => { },
+        addTransaction: async (transaction) => { },
+        updateTransaction: async (transaction) => { },
+        deleteTransaction: async (id) => { },
+        restoreBackup: async (uri) => { },
+        refresh: async () => { }
+    },
+    categories: [],
+    transactions: [],
+};
+
+export const finContext = React.createContext(initialContext);
+
+export const FinProvider = ({ children }) => {
+    const [categories, setCategories] = useState([]);
+    const [transactions, setTransactions] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const db = useRef();
+
+    useEffect(() => {
+        (async () => {
+            db.current = await openDatabase();
+            await refresh();
+            setIsLoading(false);
+        })();
+    }, [])
+
+    const restoreBackup = async (uri) => {
+        // @ts-ignore
+        db.current._db.close();
+        restoreBackup
+        await FileSystem.moveAsync({
+            from: uri,
+            to: FileSystem.documentDirectory + 'SQLite/finDatabase.db'
+        });
+        db.current = await SQLite.openDatabaseAsync('finDatabase.db');
+        db.current._db.exec(
+            [{ sql: 'PRAGMA foreign_keys = ON;', args: [] }],
+            false,
+            () => {},
+        );
+        await refresh();
+    }
+
+    const createEntity = (name, entity) => {
+        return new Promise((resolve, reject) => {
+            // create sql statement
+            let columns = '', qms = '', values = [];
+            for (const [key, value] of Object.entries(entity)) {
+                columns += `${key},`;
+                qms += '?,'
+                values.push(value)
+            }
+            columns = columns.slice(0, -1);
+            qms = qms.slice(0, -1);
+            const sql = `INSERT INTO ${name} (${columns}) VALUES (${qms})`
+            // execute sql
+            db.current.transaction(tx => {
+                tx.executeSql(sql, values,
+                    (txObj, resultSet) => {
+                        return resolve(resultSet.insertId)
+                    },
+                    (txObj, error) => {
+                        return reject(error)
+                    }
+                )
+            });
+        })
+    }
+
+    const deleteEntity = async (name, id) => {
+        await new Promise((resolve, reject) => {
+            const sql = `DELETE FROM ${name} WHERE id = ${id}`
+            // execute sql
+            db.current.transaction(tx => {
+                tx.executeSql(sql, null,
+                    (txObj, resultSet) => {
+                        return resolve()
+                    },
+                    (txObj, error) => {
+                        return reject(error)
+                    }
+                )
+            });
+        })
+    }
+
+    const addCategory = async (category) => {
+        const id = await createEntity('category', {
+            name: category.name,
+            listIndex: category.index,
+            parentId: category.parentId
+        });
+        refresh();
+        return id;
+    }
+
+    const updateCategory = async (category) => {
+        await new Promise((resolve, reject) => {
+            const sql = `UPDATE category SET 
+                    name = '${category.name}'
+                    ,listIndex = ${category.index}
+                    ,parentId = ${category.parentId}
+                WHERE id = ${category.id}`
+            // execute sql
+            db.current.transaction(tx => {
+                tx.executeSql(sql, null,
+                    (txObj, resultSet) => {
+                        return resolve()
+                    },
+                    (txObj, error) => {
+                        return reject(error)
+                    }
+                )
+            });
+        })
+        refresh();
+    }
+
+    const deleteCategory = async (id) => {
+        await deleteEntity('category', id);
+        refresh();
+    }
+
+    const addTransaction = async (transaction) => {
+        const id = await createEntity('finTransaction', {
+            name: transaction.name,
+            value: transaction.value,
+            details: transaction.details,
+            date: transaction.date.toISOString(),
+            categoryId: transaction.categoryId,
+        });
+        refresh();
+        return id;
+    }
+
+    const updateTransaction = async (transaction) => {
+        await new Promise((resolve, reject) => {
+            const sql = `UPDATE finTransaction SET 
+                    name = '${transaction.name}'
+                    ,value = ${transaction.value}
+                    ,details = '${transaction.details}'
+                    ,date = '${transaction.date.toISOString()}'
+                    ,categoryId = ${transaction.categoryId}
+                WHERE id = ${transaction.id}`
+            // execute sql
+            db.current.transaction(tx => {
+                tx.executeSql(sql, null,
+                    (txObj, resultSet) => {
+                        return resolve()
+                    },
+                    (txObj, error) => {
+                        return reject(error)
+                    }
+                )
+            });
+        })
+        refresh();
+    }
+
+    const deleteTransaction = async (id) => {
+        await deleteEntity('finTransaction', id);
+        refresh();
+    }
+
+    const fetchCategories = async (date) => {
+        await new Promise((resolve, reject) => {
+            const getCategoriesSQL = `
+            SELECT pc.*
+            ,(SELECT SUM(value)
+                FROM finTransaction
+                WHERE
+                ${date ? 'date <= \'' + date + '\' AND ' : ''} 
+                categoryId IN (
+                    WITH q AS
+                            (
+                            SELECT  id
+                            FROM    category
+                            WHERE   id = pc.id
+                            UNION ALL
+                            SELECT  c.id
+                            FROM    category c
+                            JOIN    q
+                            ON      c.parentID = q.id
+                            )
+                    SELECT  *
+                    FROM    q
+                )) AS value
+            FROM category pc
+            UNION 
+                SELECT NULL id, 'Total', 0, null
+                ,(SELECT SUM(value) FROM finTransaction ${date ? 'WHERE date <= \'' + date + '\'' : ''})
+            ORDER BY listIndex`;
+            // execute sql
+            db.current.transaction(tx => {
+                tx.executeSql(getCategoriesSQL, null,
+                    (txObj, { rows: { _array } }) => {
+                        const fetchedCategories = _array.map(c => {
+                            return {
+                                id: c.id,
+                                name: c.name,
+                                index: c.listIndex,
+                                parentId: c.parentId,
+                                value: formatValue(c.value),
+                            }
+                        })
+                        setCategories(fetchedCategories);
+                        resolve();
+                    },
+                    (txObj, error) => {
+                        return reject(error)
+                    }
+                )
+            });
+        })
+    }
+
+    const fetchTransactions = async () => {
+        await new Promise((resolve, reject) => {
+            const getTransactionsSQL = `SELECT * FROM FinTransaction ORDER BY date desc`;
+            // execute sql
+            db.current.transaction(tx => {
+                tx.executeSql(getTransactionsSQL, null,
+                    (txObj, { rows: { _array } }) => {
+                        const fetchedTransactions = _array.map(t => {
+                            return {
+                                id: t.id,
+                                name: t.name,
+                                value: formatValue(t.value),
+                                details: t.details,
+                                date: new Date(t.date),
+                                categoryId: t.categoryId
+                            }
+                        })
+                        setTransactions(fetchedTransactions);
+                        resolve();
+                    },
+                    (txObj, error) => {
+                        return reject(error)
+                    }
+                )
+            });
+        })
+    }
+
+    const refresh = async (date = null) => {
+        await fetchCategories(date);
+        await fetchTransactions();
+    }
+
+    return (
+        <finContext.Provider
+            value={{
+                actions: {
+                    refresh,
+                    addCategory,
+                    updateCategory,
+                    deleteCategory,
+                    addTransaction,
+                    updateTransaction,
+                    deleteTransaction,
+                    restoreBackup
+                },
+                isLoading,
+                categories,
+                transactions
+            }}
+        >
+            {children}
+        </finContext.Provider>
+    );
+};
